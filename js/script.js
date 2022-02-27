b9bdaaa4f574a480a97e026c7d82878d2720b925
Leonardo Villalobos <levillalobos@csumb.edu>
2022-02-26 21:45:00 -0800

    ➕ Add buttons visual effects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-let cartButton = document.querySelector("#goto-cart");
-cartButton.addEventListener("click", sayHello);
+const COURSES = ["C++", "Java", "Python", "Rust", "JavaScript"];
+let enrollments = [];
 
-function sayHello() {
-console.log("Hello");
+let cartButton = document.getElementById("goto-cart");
+cartButton.addEventListener("click", () => {
+    console.log("Hello");
+});
+
+function enroll() {
+    console.log(this);
+    if (enrollments.indexOf(this.value) == -1) {
+        enrollments.push(this.value);
+        this.classList.remove("btn-outline-warning");
+        this.classList.add("btn-success");
+        this.innerHTML = "Added to cart";
+    } else {
+        enrollments.splice(enrollments.indexOf(this.value), 1);
+        this.classList.remove("btn-success");
+        this.classList.add("btn-outline-warning");
+        this.innerHTML = "Enroll";
+    }
+    console.log(`Courses: [${enrollments}]`);
 }
+
+let enrollmentSelector = document.querySelectorAll(".enrollment");
+enrollmentSelector.forEach(child => {
+    child.addEventListener("click", enroll);
+});
